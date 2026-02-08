647d36b3715c3b6febeca8a6fb468c334cfd3d05
Gonzalo Atienza <gonatienza@gmail.com>
2026-02-07 20:05:24 -0500

    cmd

--- tests/manual/TPC.cmd
+++ tests/manual/TPC.cmd
@@ -3,6 +3,7 @@ set pmnewpass=mTiIqTr2fO1YIh2Lmgyx
 set pmextrapass1=gqYJ96jipQpimq
 set pmextrapass3=27Fp28KdWravJZIAL0VZ
 bin\CyberArk.TPC.exe User.ini logon
+bin\CyberArk.TPC.exe User.ini verifypass
 bin\CyberArk.TPC.exe User.ini changepass
 bin\CyberArk.TPC.exe User.ini prereconcilepass
 bin\CyberArk.TPC.exe User.ini reconcilepass
