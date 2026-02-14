fcea8759a26dcf2f815b18b5e037c4a44d2bc5c6
Gonzalo Atienza <38573982+gonatienza@users.noreply.github.com>
2026-02-14 17:09:40 -0500

    Reorder commands in TPC.cmd for clarity

--- tests/manual/TPC.cmd
+++ tests/manual/TPC.cmd
@@ -2,8 +2,8 @@ set pmpass=p8RaRY9Rd4OnShpc1WYU
 set pmnewpass=mTiIqTr2fO1YIh2Lmgyx
 set pmextrapass1=gqYJ96jipQpimq
 set pmextrapass3=27Fp28KdWravJZIAL0VZ
-bin\CyberArk.TPC.exe User.ini logon
 bin\CyberArk.TPC.exe User.ini verifypass
+bin\CyberArk.TPC.exe User.ini logon
 bin\CyberArk.TPC.exe User.ini changepass
 bin\CyberArk.TPC.exe User.ini prereconcilepass
 bin\CyberArk.TPC.exe User.ini reconcilepass
