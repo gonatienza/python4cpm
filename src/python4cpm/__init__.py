a9dbc4aea4a0452b945739a86c59691a7ec45f0e
Gonzalo Atienza <gonatienza@gmail.com>
2026-02-16 19:08:05 -0500

    pipy

--- src/python4cpm/__init__.py
+++ src/python4cpm/__init__.py
@@ -1,5 +1,5 @@
-from .python4cpm import Args, Secret, Secrets, Python4CPM
-from .tpchelper import TPCHelper
+from python4cpm.python4cpm import Args, Secret, Secrets, Python4CPM
+from python4cpm.tpchelper import TPCHelper
 from importlib.metadata import version as __version
 
 __version__ = __version(__name__)
